e6dd977362a6d929946ad3d1eedaf2695db94888
Pedro Ayres <pedroayresb@gmail.com>
2023-02-27 16:59:28 -0400

    fix api lint

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,11 +1,9 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export default () => {
-  return axios.create({
-    baseURL: 'http://localhost:3001',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-      'Access-Control-Allow-Origin': '*'
-    }
-  })
-}
+export default () => axios.create({
+  baseURL: 'http://localhost:3001',
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+    'Access-Control-Allow-Origin': '*',
+  },
+});
