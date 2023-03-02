1ec24c25384e99537275c3d0e6ce47ace08941dd
Ruham Leal <ruhamxlpro@hotmail.com>
2023-03-01 22:56:10 -0300

    feat: Adding Register Route and conecting in the app

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -3,14 +3,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const loginRoutes = require('./routes/login.routes');
+const registerRoutes = require('./routes/register.routes');
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Its Working');
 });
 
 app.use('/login', loginRoutes);
+
+app.use('/register', registerRoutes);
 
 module.exports = app;
