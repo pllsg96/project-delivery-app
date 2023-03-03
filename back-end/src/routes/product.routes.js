5a4619e6c211866d058b7355b497a3134dc47af8
pedroygor <pedroygor2010@hotmail.com>
2023-03-03 05:39:23 -0300

    fix: export of product route

--- back-end/src/routes/product.routes.js
+++ back-end/src/routes/product.routes.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
-const controller = require('../controller/product.controller');
 
 const router = Router();
 
+const controller = require('../controller/product.controller');
+
 router.get('/', controller.getAllProducts);
+
+module.exports = router;
