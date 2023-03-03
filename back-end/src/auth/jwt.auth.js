68707c3e39336b11116640378ed0de5df454dd2b
Pedro Luiz <pedroluizlsg@gmail.com>
2023-03-03 18:42:44 -0300

    feat: integração frontend e backend para rota products

--- back-end/src/auth/jwt.auth.js
+++ back-end/src/auth/jwt.auth.js
@@ -1,12 +1,22 @@
 const jwt = require('jsonwebtoken');
+const path = require('path');
+require('dotenv').config();
 
-const header = { expiresIn: '30m', algorithm: 'HS256' };
+const jwtSecretPath = path.join(__dirname, '../../jwt.evaluation.key');
 
-const jwtSecret = '123456';
+const jwtSecret = require("fs")
+  .readFileSync(jwtSecretPath, { encoding: "utf-8" });
+
 
 const createToken = (user) => {
-    const token = jwt.sign({ data: user }, jwtSecret, header);
-    return token;
+  console.log(__dirname);
+  const header = {
+    expiresIn: '1d',
+    algorithm: 'HS256',
+  };
+
+  const token = jwt.sign({ data: user }, jwtSecret, header);
+  return token;
 };
 
 const validateToken = (token) => {
--- front-end/src/components/ProductComponents/CheckoutButton.jsx
+++ front-end/src/components/ProductComponents/CheckoutButton.jsx
@@ -1,23 +1,28 @@
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import DeliveryAppContext from '../../context/DeliveryAppContext';
 
 function CheckoutButton() {
+  const history = useHistory();
   const { price, cartQuantity } = useContext(DeliveryAppContext);
 
   return (
     <button
       type="button"
+      onClick={ () => history.push('/customer/checkout') }
       data-testid="customer_products__button-cart"
       disabled={ price === 0 }
     >
-      <p
-        data-testid="customer_products__checkout-bottom-value"
-      >
+      <p>
         { cartQuantity > 0 && cartQuantity }
         {' '}
         Ver Carrinho: R$
         {' '}
-        { Number(price).toFixed(2) }
+      </p>
+      <p
+        data-testid="customer_products__checkout-bottom-value"
+      >
+        { (Number(price).toFixed(2)).replace('.', ',') }
       </p>
     </button>
   );
