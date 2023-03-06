1e1c7dfc20b6009702c42be16471fde5657514aa
Pedro Luiz <pedroluizlsg@gmail.com>
2023-03-06 15:13:49 -0300

    fix: correção do requisito 13

--- front-end/src/pages/LoginPage.jsx
+++ front-end/src/pages/LoginPage.jsx
@@ -1,104 +1,34 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import fetchAPI from '../services/fetchAPI';
-import statusCode from '../utils/statusCode';
+import PropTypes from 'prop-types';
+import ErrorMessage from '../components/ErrorMessage';
+import LoginButton from '../components/LoginComponents/LoginButton';
+import LoginForm from '../components/LoginComponents/LoginForm';
 
-function LoginPage() {
+function LoginPage(props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(false);
-  const history = useHistory();
+  const [error, setError] = useState('');
 
-  const validateEmail = (emailToValidate) => {
-    const EMAIL_REGEX = /\S+@\S+\.\S+/;
-    const isValidate = EMAIL_REGEX.test(emailToValidate);
-    return isValidate;
-  };
-
-  const validatePassword = (passwordToValidate) => {
-    const MIN_LENGTH_PASSWORD = 6;
-    const isValidate = passwordToValidate.length >= MIN_LENGTH_PASSWORD;
-    return isValidate;
-  };
-
-  const handleLogin = async (event) => {
-    event.preventDefault();
-
-    const response = await fetchAPI.loginRequisition(email, password);
-    const { status } = response;
-
-    if (status === statusCode.NOT_FOUND) {
-      setError(true);
-    } else if (status === statusCode.OK) {
-      localStorage.setItem('user', JSON.stringify(response));
-      history.push('/customer/products');
-    } else {
-      setError(true);
-    }
-  };
-
-  const handleRegister = () => {
-    history.push('/register');
-  };
-
-  const handleEmail = ({ target }) => {
-    setEmail(target.value);
-  };
-
-  const handlePassword = ({ target }) => {
-    setPassword(target.value);
-  };
+  const { history } = props;
 
   return (
-    <div>
-      <form>
-        <label htmlFor="email">
-          Login
-          <input
-            type="email"
-            data-testid="common_login__input-email"
-            id="email"
-            value={ email }
-            onChange={ handleEmail }
-          />
-        </label>
-
-        <label htmlFor="password">
-          Senha
-          <input
-            type="password"
-            data-testid="common_login__input-password"
-            id="password"
-            value={ password }
-            onChange={ handlePassword }
-          />
-        </label>
-
-        <button
-          type="submit"
-          data-testid="common_login__button-login"
-          disabled={ !validateEmail(email) || !validatePassword(password) }
-          onClick={ handleLogin }
-        >
-          Login
-        </button>
-        <button
-          type="button"
-          data-testid="common_login__button-register"
-          onClick={ handleRegister }
-        >
-          Cadastrar
-        </button>
-      </form>
-      {error && (
-        <p
-          data-testid="common_login__element-invalid-email"
-        >
-          Email e/ou Password Inválidos
-        </p>
-      )}
-    </div>
+    <form>
+      <LoginForm setEmail={ setEmail } setPassword={ setPassword } />
+      <LoginButton
+        email={ email }
+        password={ password }
+        setError={ setError }
+        history={ history }
+      />
+      { error.length > 0 && <ErrorMessage error={ error } from="login" /> }
+    </form>
   );
 }
+
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default LoginPage;
