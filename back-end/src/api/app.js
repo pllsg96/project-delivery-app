49a83afaeafe5768efb480627d9a916a8035d18f
pedroygor <pedroygor2010@hotmail.com>
2023-03-06 01:16:44 -0300

    chore: changing directory files

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const app = express();
 const cors = require('cors');
-const loginRoutes = require('./routes/login.routes');
-const registerRoutes = require('./routes/register.routes');
-const productsRoutes = require('./routes/product.routes');
+const loginRoutes = require('../routes/login.routes');
+const registerRoutes = require('../routes/register.routes');
+const productsRoutes = require('../routes/product.routes');
 
 app.use(express.json());
 app.use(cors());
 app.use('/images', express.static('public'));
+
+app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.get('/', (_req, res) => {
   res.send('Its Working');
