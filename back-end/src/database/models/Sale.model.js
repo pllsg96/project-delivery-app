c5a38111b6170b4c8d5a10551c79d794b2002c04
pedroygor <pedroygor2010@hotmail.com>
2023-03-07 00:24:54 -0300

    refactor: remove allowNull

--- back-end/src/database/models/Sale.model.js
+++ back-end/src/database/models/Sale.model.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
     totalPrice: { type: DataTypes.DECIMAL, allowNull: false },
     deliveryAddress: { type: DataTypes.STRING, allowNull: false },
     deliveryNumber: { type: DataTypes.STRING, allowNull: false },
-    saleDate: { type: DataTypes.DATE, defaultValue: DataTypes.NOW, allowNull: false },
+    saleDate: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
     status: { type: DataTypes.STRING, allowNull: false },
   }, {
     underscored: true,
